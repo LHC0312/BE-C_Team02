b5e2e4cb7058e9b1284510fac2d1fceb27d496b1
LHC0312 <chan050312@gmail.com>
2024-07-16 21:29:33 +0900

    INIT: 프로젝트 업로드

---
+++ ITS_Back-End-main/src/main/java/ICT_Team2/ITS_Back_End_main/ItsBackEndMainApplication.java
@@ -0,0 +1,13 @@
+package ICT_2_Team.ITS_Back_End_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsBackEndMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsBackEndMainApplication.class, args);
+	}
+
+}
---
+++ ITS_Back-End-main/src/test/java/ICT_2_Team/ITS_Back_End_main/ItsBackEndMainApplicationTests.java
@@ -0,0 +1,13 @@
+package ICT_2_Team.ITS_Back_End_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsBackEndMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
