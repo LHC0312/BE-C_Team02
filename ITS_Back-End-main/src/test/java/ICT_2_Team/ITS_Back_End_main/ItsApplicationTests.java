bb3e6248e69e1bbb2b55b46cb092d18d53e8a151
LHC0312 <chan050312@gmail.com>
2024-07-26 08:56:00 +0900

    FIX: Test 폴더 수정

--- ITS_Back-End-main/src/test/java/ICT_2_Team/ITS_Back_End_main/ItsApplicationTests.java
+++ ITS_Back-End-main/src/test/java/ICT_2_Team/ITS_Back_End_main/ItsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ItsApplicationTests {
+class ItsBackEndMainApplicationTests {
 
 	@Test
 	void contextLoads() {
