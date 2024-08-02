d1d8c7b2f7456e209b757e2766efdb8fc9f4bbe3
haeun030 <haeun14_0_0@naver.com>
2024-08-02 21:30:56 +0900

    ADD:CommentDto

--- ITS_Back-End-main/src/main/java/ICT_Team2/ITS_Back_End_main/web/controller/CommentRestController.java
+++ ITS_Back-End-main/src/main/java/ICT_Team2/ITS_Back_End_main/web/controller/CommentRestController.java
@@ -1,6 +1,12 @@
 package ICT_Team2.ITS_Back_End_main.web.controller;
 
+import ICT_Team2.ITS_Back_End_main.apiPayLoad.ApiResponse;
+import ICT_Team2.ITS_Back_End_main.web.dto.CommentRequestDTO;
+import ICT_Team2.ITS_Back_End_main.web.dto.CommentResponseDTO;
+import ICT_Team2.ITS_Back_End_main.web.dto.CommentResponseDTO.ResponseDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
