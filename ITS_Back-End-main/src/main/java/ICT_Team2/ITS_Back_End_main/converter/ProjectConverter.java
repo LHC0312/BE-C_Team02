c52823670e048130bd1a51a3e52092aa0c21e943
XiBBaL <crosgame1128@gmail.com>
2024-08-26 08:38:19 +0900

    FEAT : Project Converter

--- ITS_Back-End-main/src/main/java/ICT_Team2/ITS_Back_End_main/converter/ProjectConverter.java
+++ ITS_Back-End-main/src/main/java/ICT_Team2/ITS_Back_End_main/converter/ProjectConverter.java
@@ -1,4 +1,36 @@
 package ICT_Team2.ITS_Back_End_main.converter;
 
+import org.springframework.stereotype.Component;
+
+import ICT_Team2.ITS_Back_End_main.web.dto.ProjectRequestDTO;
+import ICT_Team2.ITS_Back_End_main.web.dto.ProjectResponseDTO;
+
+@Component
 public class ProjectConverter {
+
+    public ProjectResponseDTO toResponseDto(Project project) {
+        return new ProjectResponseDTO(
+                project.getId(),
+                project.getName(),
+                project.getIsCreated(),
+                project.getStatus(),
+                project.getCreatedAt(),
+                project.getInactiveDate(),
+                project.getIsDeleted(),
+                project.getUpdatedAt()
+        );
+    }
+
+    public Project toEntity(ProjectRequestDTO projectRequestDTO) {
+        Project project = new Project();
+        project.setName(projectRequestDTO.getName());
+        project.setIsCreated(projectRequestDTO.getIsCreated());
+        project.setStatus(projectRequestDTO.getStatus());
+        project.setCreatedAt(projectRequestDTO.getCreatedAt());
+        project.setInactiveDate(projectRequestDTO.getInactiveDate());
+        project.setIsDeleted(projectRequestDTO.getIsDeleted());
+        project.setUpdatedAt(projectRequestDTO.getUpdatedAt());
+        return project;
+    }
 }
+
